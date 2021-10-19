7f63d36a981f364163d6e80d56abbbafc06c6a7c
Filip Dorian Pindej <fpindej@users.noreply.github.com>
2021-10-19 20:09:08 +0200

    Initial commit

---
+++ src/Playground.App/Playground.App/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Playground.App.ViewModels;
+using Playground.App.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace Playground.App
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
---
+++ src/Playground.App/Playground.App/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Playground.App.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Playground.App.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
